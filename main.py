d460554039bd68d9656e343c65df69d4c4d685b5
Vishwajith Shaijukumar <83128994+root-cyborg127@users.noreply.github.com>
2022-06-24 23:27:34 +0530

    updated

--- main.py
+++ main.py
@@ -14,7 +14,14 @@ from phonenumbers import timezone
 
 from phonenumbers import geocoder
 
-
+from rich import *
+from rich.progress import track
+from time import sleep
+import string 
+import random
+from rich import style
+import copy
+from rich import print
 hostname = socket.gethostname()
 IPAddr = socket.gethostbyname(hostname)
 
@@ -82,9 +89,9 @@ print()
 print()
 
 print('\u001b[33;1m-----------------------------------------------------------------------------------------------------------------------------------------------------------\u001b[0m'), time.sleep(.5)
-print('\u001b[32m[1]\u001b[0m IPLOCATOR    - Locates any device using PUBLIC IP Address                   \u001b[32m[8]\u001b[0m Coming Soon🔥'), time.sleep(.1)
-print('\u001b[32m[2]\u001b[0m PHONE-INFO>>>    - INFO GATHERING OF PHONENUMBER [BASIC]          \u001b[32m[9]\u001b[0m Coming Soon🔥'), time.sleep(.1)
-print('\u001b[32m[3]\u001b[0m Coming Soon🔥                                                        \u001b[32m[10]\u001b[0m Coming Soon🔥'), time.sleep(.1)
+print('\u001b[32m[1]\u001b[0m IPLOCATOR    - Locates any device using PUBLIC IP Address            \u001b[32m[8]\u001b[0m Coming Soon🔥'), time.sleep(.1)
+print('\u001b[32m[2]\u001b[0m PHONE-INFO>>>    - INFO GATHERING OF PHONENUMBER [BASIC]             \u001b[32m[9]\u001b[0m Coming Soon🔥'), time.sleep(.1)
+print('\u001b[32m[3]\u001b[0m SNEEZ            - Random PASSWD GENERATOR                           \u001b[32m[10]\u001b[0m Coming Soon🔥'), time.sleep(.1)
 print('\u001b[32m[4]\u001b[0m Coming Soon🔥                                                        \u001b[32m[11]\u001b[0m Coming Soon🔥'), time.sleep(.1)
 print('\u001b[32m[5]\u001b[0m Coming Soon🔥                                                        \u001b[32m[12]\u001b[0m Coming Soon🔥'), time.sleep(.1)
 print('\u001b[32m[6]\u001b[0m Coming Soon🔥                                                        \u001b[32m[13]\u001b[0m Coming Soon🔥'), time.sleep(.1)
@@ -233,9 +240,9 @@ def SOON():
     sys.exit()
 def OPTION_LIST():
     print('\u001b[33;1m-----------------------------------------------------------------------------------------------------------------------------------------------------------\u001b[0m'), time.sleep(.5)
-    print('\u001b[32m[1]\u001b[0m IPLOCATOR    - Locates any device using PUBLIC IP Address                   \u001b[32m[8]\u001b[0m Coming Soon🔥'), time.sleep(.1)
-    print('\u001b[32m[2]\u001b[0m PHONE-INFO>>>    - INFO GATHERING OF PHONENUMBER [BASIC]           \u001b[32m[9]\u001b[0m Coming Soon🔥'), time.sleep(.1)
-    print('\u001b[32m[3]\u001b[0m Coming Soon🔥                                                        \u001b[32m[10]\u001b[0m Coming Soon🔥'), time.sleep(.1)
+    print('\u001b[32m[1]\u001b[0m IPLOCATOR    - Locates any device using PUBLIC IP Address            \u001b[32m[8]\u001b[0m Coming Soon🔥'), time.sleep(.1)
+    print('\u001b[32m[2]\u001b[0m PHONE-INFO>>>    - INFO GATHERING OF PHONENUMBER [BASIC]             \u001b[32m[9]\u001b[0m Coming Soon🔥'), time.sleep(.1)
+    print('\u001b[32m[3]\u001b[0m SNEEZ            - Random PASSWD GENERATOR                           \u001b[32m[10]\u001b[0m Coming Soon🔥'), time.sleep(.1)
     print('\u001b[32m[4]\u001b[0m Coming Soon🔥                                                        \u001b[32m[11]\u001b[0m Coming Soon🔥'), time.sleep(.1)
     print('\u001b[32m[5]\u001b[0m Coming Soon🔥                                                        \u001b[32m[12]\u001b[0m Coming Soon🔥'), time.sleep(.1)
     print('\u001b[32m[6]\u001b[0m Coming Soon🔥                                                        \u001b[32m[13]\u001b[0m Coming Soon🔥'), time.sleep(.1)
@@ -250,6 +257,114 @@ def support():
   print("\u001b[36m INSTAGRAM:- https://instagram.com/vishwajithshaijukumar ")
   print("\u001b[35mTELEGRAM:- https://telegram.me/milliesgreatfan")
   print("\u001b[33;1m-------------------------------------------------------[[reset]]")
+
+  sys.exit()
+    
+def banner():
+
+ print(""" [blue]
+
+      ::::::::  ::::    ::: :::::::::: :::::::::: ::::::::: 
+    :+:    :+: :+:+:   :+: :+:        :+:             :+:   
+   +:+        :+:+:+  +:+ +:+        +:+            +:+     
+  +#++:++#++ +#+ +:+ +#+ +#++:++#   +#++:++#      +#+       
+        +#+ +#+  +#+#+# +#+        +#+          +#+         
+#+#    #+# #+#   #+#+# #+#        #+#         #+#           
+########  ###    #### ########## ########## #########       
+
+[/blue]    
+""")
+ print("""[red] RANDOM PASSWD GENERATOR *[/red]""")
+sleep(0.01)
+
+
+for _ in track(range(100), description='[green]Processing '):
+
+
+
+
+
+
+
+  
+    
+ alphabets = list(string.ascii_letters)
+digits = list(string.digits)
+special_characters = list("!@#$%^&*()")
+characters_count = list(string.ascii_letters + string.digits + "!@#$%^&*()_")
+
+def generate_random_password():
+	
+	length = int(input("Enter password length >> "))
+	print()
+
+	print("[#9D00FF]+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++[/#9D00FF]")
+
+
+	alphabets_count =int(input("Enter alphabets count in password: ")) 
+	print()
+
+
+	print("[#9D00FF]===============================================================================================[/#9D00FF]")
+	digits_count = int(input("Enter digits count in password: "))
+	print()
+
+
+	print("[#9D00FF]++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++[/#9D00FF]")
+
+	special_characters_count = int(input("Enter special characters count in password: "))
+
+	print()
+
+
+	print("[#9D00FF]==================================================================================================[/#9D00FF]")
+
+	characters_count = alphabets_count + digits_count + special_characters_count
+
+	if characters_count > length:
+		print("Characters total count is greater than the password length")
+		return
+
+	password = []
+	
+	
+	for i in range(alphabets_count):
+		password.append(random.choice(alphabets))
+
+
+	for i in range(digits_count):
+		password.append(random.choice(digits))
+
+
+	for i in range(special_characters_count):
+		password.append(random.choice(special_characters))
+
+	if characters_count < length:
+		random.shuffle(characters)
+		for i in range(length - characters_count):
+			password.append(random.choice(characters))
+
+
+	random.shuffle(password)
+	print("[#F433FF]YOUR GEN PASSWD IS >>>[/#F433FF]")
+	print()
+
+
+
+	print()
+	
+	print("[#00FFFF]""".join(password))
+	print()
+
+
+	print("[#6AFB92]#########################################################################################################[/#6AFB92]")
+
+print("[green][+][/green] [bold magenta]Password generated ![/bold magenta]!", ":white_check_mark:")
+print("[#6AFB92]#########################################################################################################[/#6AFB92]")     
+
+
+
+
 
 
 
@@ -279,7 +394,10 @@ while True:
              
              if usrInp == "3":
                 
-               SOON()
+                banner()
+               
+                generate_random_password()
+               
                 
              if usrInp == "4":
                 
